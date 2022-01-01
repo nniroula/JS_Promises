157c05e6d3f061138d2678d525c2458709c1b3b4
nabin <nkbhaie2017@gmail.com>
2022-01-01 16:36:01 -0700

    finish working in js file

--- Number_Facts/async_await_nums.js
+++ Number_Facts/async_await_nums.js
@@ -2,6 +2,7 @@
 // part 1
 const BASE_URL = "http://numbersapi.com";
 let favoriteNumber = 42;
+
 async function getFavNumber(){
     // let favNum = await axios.get(`${BASE_URL}/${favoriteNumber}?json`);
     // apply object destructuring here
@@ -11,49 +12,41 @@ async function getFavNumber(){
     // console.log(localVariable);
     console.log(localVariable.text);
 }
+/*
+*/
+// part 2
+let multipleNums = [1, 3, 5];
+async function getFactsOfMultipleNums(){
+    let multipleNumFacts = await axios.get(`${BASE_URL}/${multipleNums}?json`);
+   // Try destructuring
+//    let {data: res1, data: res2, data: res3} = await axios.get(`${BASE_URL}/${multipleNums}?json`);
 
+    console.log(multipleNumFacts);
+    // console.log(multipleNumFacts.data[0].text); // this does not work
+    // console.log(res1);
+    // console.log(res2);
+    // console.log(res3);
+}
 
+// part 3, four facts about a number
+async function fourFactsAboutNum(){
+    let facts = await Promise.all(
+    // await Promise.all(
+        // Array.from({ length: 4}, () => {return axios.get(`${BASE_URL}/${favoriteNumber}?json`)})
+        // Array.from({ length: 4}, () => axios.get(`${BASE_URL}/${favoriteNumber}?json`))
+        Array.from({ length: 4}, () => {return $.getJSON(`${BASE_URL}/${favoriteNumber}?json`)})
 
-
-
-
-
-
-
+        );
+        facts.forEach(data => {
+            $('body').append(`<p> ${data.text}</p>`);
+        });
+}
 
 
 // ######################################
-
-/* This exercise is done using promise */
-/*
-const BASE_URL = "http://numbersapi.com"
-let favorite_number = 42;
-// function number_fact(){
-//1
-// let firstPromise = axios.get("http://numbersapi.com/42?json");
-let firstPromise = axios.get(`${BASE_URL}/${favorite_number}?json`);
-// resolved holds the data that the .then() function returns //.data.text comes from the API's data response
-firstPromise.then(resolved => console.log(resolved));
-// OR
-// $.getJSON(`${baseURL}/${favNumber}?json`).then(data => {
-//     console.log(data);
-//   });
-
-// 2.
-// for multiple numbers, and also range of some numbers
-// http://numbersapi.com/1..3,10 
-let favorite_nums = [7, 11, 22];
-$.getJSON(`${BASE_URL}/${favorite_nums}?json`).then(res => {
-console.log(res);
-});
-
-// 3.
-Promise.all(
-    Array.from({ length: 4 }, () => {
-    return $.getJSON(`${BASE_URL}/${favorite_number}?json`);
-    })
-).then(facts => {
-    facts.forEach(res => $("body").append(`<p>${res.text}</p>`));
-});
-// }
+/** 
+ in console, do the following
+ getFavNumber()
+ getFactsOfMultipleNums()
+ fourFactsAboutNum()
 */
