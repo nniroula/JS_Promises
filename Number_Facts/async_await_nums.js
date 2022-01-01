92c7e88cfff1c57c8c7792c67e3272e7fe427635
nabin <nkbhaie2017@gmail.com>
2022-01-01 15:45:49 -0700

    add js file to work with async and await

---
+++ Number_Facts/async_await_nums.js
@@ -0,0 +1,59 @@
+// async await
+// part 1
+const BASE_URL = "http://numbersapi.com";
+let favoriteNumber = 42;
+async function getFavNumber(){
+    // let favNum = await axios.get(`${BASE_URL}/${favoriteNumber}?json`);
+    // apply object destructuring here
+    let {data: localVariable} = await axios.get(`${BASE_URL}/${favoriteNumber}?json`);
+
+    // console.log(favNum);
+    // console.log(localVariable);
+    console.log(localVariable.text);
+}
+
+
+
+
+
+
+
+
+
+
+
+// ######################################
+
+/* This exercise is done using promise */
+/*
+const BASE_URL = "http://numbersapi.com"
+let favorite_number = 42;
+// function number_fact(){
+//1
+// let firstPromise = axios.get("http://numbersapi.com/42?json");
+let firstPromise = axios.get(`${BASE_URL}/${favorite_number}?json`);
+// resolved holds the data that the .then() function returns //.data.text comes from the API's data response
+firstPromise.then(resolved => console.log(resolved));
+// OR
+// $.getJSON(`${baseURL}/${favNumber}?json`).then(data => {
+//     console.log(data);
+//   });
+
+// 2.
+// for multiple numbers, and also range of some numbers
+// http://numbersapi.com/1..3,10 
+let favorite_nums = [7, 11, 22];
+$.getJSON(`${BASE_URL}/${favorite_nums}?json`).then(res => {
+console.log(res);
+});
+
+// 3.
+Promise.all(
+    Array.from({ length: 4 }, () => {
+    return $.getJSON(`${BASE_URL}/${favorite_number}?json`);
+    })
+).then(facts => {
+    facts.forEach(res => $("body").append(`<p>${res.text}</p>`));
+});
+// }
+*/
