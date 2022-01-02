3638c45378ff0e11c3a72a9780ca3c7b74718a86
nabin <nkbhaie2017@gmail.com>
2022-01-01 18:08:16 -0700

    add more functions to js file

--- Deck_of_cards/async_await.js
+++ Deck_of_cards/async_await.js
@@ -1,5 +1,6 @@
 // part 1
-let baseUrl = "http://deckofcardsapi.com/api/deck/";
+let baseUrl = "http://deckofcardsapi.com/api/deck";
+
 async function singleCard(){
     let oneCardSuffl = await axios.get(`${baseUrl}/new/draw/`);
     // destructure to get the needed values
@@ -8,58 +9,42 @@ async function singleCard(){
     console.log(`${suit}, ${value}`);
 }
 
+// part 2
+async function newlySuffledCard(){
+    let newSuffledCardData = await axios.get(`${baseUrl}/new/draw/`);
+    // console.log(newSuffledCard.data.deck_id);
+    let deckId = newSuffledCardData.data.deck_id;
+    let secondSuffledCardData = await axios.get(`${baseUrl}/${deckId}/draw/`);
+    [newSuffledCardData, secondSuffledCardData].forEach(val => {
+        // destructure here to get back certain data from whole list of data
+        let {suit, value} = val.data.cards[0];
+        console.log(`${suit}, ${value}`);
+    })
+    // console.log(secondSuffledCardData);
+}
 
+/* part 3 is Not yet fully functional */
+// part 3 
+async function htmlCardDraw(){
+    let $button = $('button');  // retrieve button
+    let $cardArea = $('#card-area'); // retrieve cardArea
 
+    let dataFromDeck = await axios.get(`${baseUrl}/new/shuffle/`);
 
-
-// ####################################
-/*
-
-$(function() {
-    let baseURL = 'https://deckofcardsapi.com/api/deck';
-  
-    // 1.
-    async function part1() {
-      let data = await $.getJSON(`${baseURL}/new/draw/`);
-      let { suit, value } = data.cards[0];
-      console.log(`${value.toLowerCase()} of ${suit.toLowerCase()}`);
-    }
-  
-    // 2.
-    async function part2() {
-      let firstCardData = await $.getJSON(`${baseURL}/new/draw/`);
-      let deckId = firstCardData.deck_id;
-      let secondCardData = await $.getJSON(`${baseURL}/${deckId}/draw/`);
-      [firstCardData, secondCardData].forEach(card => {
-        let { suit, value } = card.cards[0];
-        console.log(`${value.toLowerCase()} of ${suit.toLowerCase()}`);
-      });
-    }
-  
-    // 3.
-    async function setup() {
-      let $btn = $('button');
-      let $cardArea = $('#card-area');
-  
-      let deckData = await $.getJSON(`${baseURL}/new/shuffle/`);
-      $btn.show().on('click', async function() {
-        let cardData = await $.getJSON(`${baseURL}/${deckData.deck_id}/draw/`);
+    $button.show().on('click', async function(){
+        let cardData = await $.getJSON(`${baseUrl}/${dataFromDeck.deck_id}/draw/`);
         let cardSrc = cardData.cards[0].image;
         let angle = Math.random() * 90 - 45;
         let randomX = Math.random() * 40 - 20;
         let randomY = Math.random() * 40 - 20;
         $cardArea.append(
-          $('<img>', {
-            src: cardSrc,
-            css: {
-              transform: `translate(${randomX}px, ${randomY}px) rotate(${angle}deg)`
-            }
-          })
+            $('<img>', {
+                src: cardSrc,
+                css:{
+                    transform: `translate(${randomX}px, ${randomY}px) rotate(${angle}deg)`
+                }
+            })
         );
         if (cardData.remaining === 0) $btn.remove();
-      });
-    }
-    setup();
-  });
-  
-*/
+    });  
+}
